0a106a76fba0184d3a1a1ee427fc63955429a3c6
Ana <lipcanana1997@gmail.com>
2022-03-11 23:54:22 +0200

    Add of StudentDecis Class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,11 @@
 package com.company;
+import com.company.libs.StudentDecis;
 
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World! New");
+
+        StudentDecis student1 = new StudentDecis("Vasile", 123456789);
     }
 }
---
+++ src/com/company/libs/StudentDecis.java
@@ -0,0 +1,12 @@
+package com.company.libs;
+
+public class StudentDecis {
+    String nume;
+    int idnp;
+    public StudentDecis (String numeAtribut, int idnpAtribut){
+        nume = numeAtribut;
+        this.idnp = idnpAtribut;
+    };
+
+
+}
